92b078b14c210aa0dcdef8bad78626219d762c7f
Harshna Goratela <harshna.goratela@gmail.com>
2021-02-21 13:01:26 +0530

    gtag configures

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,13 +68,17 @@ module.exports = {
         showSpinner: false,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-tagmanager`,
-    //   options: {
-    //     id: process.env.GTM_ID,
-    //     includeInDevelopment: false,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-W44XE0GV18", // Google Analytics / GA
+          //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
