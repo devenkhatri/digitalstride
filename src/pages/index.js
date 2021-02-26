b4132ebc017d174d549db7cbde8f6888e4b2e6ab
dg <deven.khatri@gmail.com>
2021-02-26 16:47:19 +0530

    tech stack section added

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,18 +21,20 @@ const IndexPage = (props) => {
       });
     });
   }
-  // const offerings = [
-  //   {title: "one", image: "https://shuffle.dev/metis-assets/illustrations/work-tv.png", url:"/", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit. Pretium lectus quam id leo in vitae turpis massa sed. Duis at tellus at urna condimentum mattis pellentesque id. Velit egestas dui id ornare."},
-  //   {title: "two", image: "https://shuffle.dev/metis-assets/illustrations/people-watching.png", url:"/", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit. Pretium lectus quam id leo in vitae turpis massa sed. Duis at tellus at urna condimentum mattis pellentesque id. Velit egestas dui id ornare."},
-  //   {title: "three", image: "https://bulma.dev/placeholder/pictures/bg_4-3.svg?primary=ffd1b2", url:"/", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit. Pretium lectus quam id leo in vitae turpis massa sed. Duis at tellus at urna condimentum mattis pellentesque id. Velit egestas dui id ornare."},
-  //   {title: "four", image: "https://shuffle.dev/metis-assets/illustrations/financial-report.png", url:"/", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit. Pretium lectus quam id leo in vitae turpis massa sed. Duis at tellus at urna condimentum mattis pellentesque id. Velit egestas dui id ornare."},
-  // ];
-  // const offerings2 = [
-  //   {title: "five", image: "https://bulma.dev/placeholder/pictures/bg_4-3.svg?primary=11ffb2", url:"/", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit. Pretium lectus quam id leo in vitae turpis massa sed. Duis at tellus at urna condimentum mattis pellentesque id. Velit egestas dui id ornare."},
-  //   {title: "six", image: "https://bulma.dev/placeholder/pictures/bg_4-3.svg?primary=11d1ff", url:"/", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit. Pretium lectus quam id leo in vitae turpis massa sed. Duis at tellus at urna condimentum mattis pellentesque id. Velit egestas dui id ornare."},
-  //   {title: "seven", image: "https://bulma.dev/placeholder/pictures/bg_4-3.svg?primary=ffd1b2", url:"/", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit. Pretium lectus quam id leo in vitae turpis massa sed. Duis at tellus at urna condimentum mattis pellentesque id. Velit egestas dui id ornare."},
-  //   {title: "eight", image: "https://bulma.dev/placeholder/pictures/bg_4-3.svg?primary=44f1b4", url:"/", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit. Pretium lectus quam id leo in vitae turpis massa sed. Duis at tellus at urna condimentum mattis pellentesque id. Velit egestas dui id ornare."},
-  // ];
+  const logos = [
+    {title: "AEM", image: "/images/worklogos/aem.png", url:"/"},
+    {title: "WooCommerce", image: "/images/worklogos/woocommerce.png", url:"/"},
+    {title: "Magento", image: "/images/worklogos/magento.png", url:"/"},
+    {title: "Wordpress", image: "/images/worklogos/wordpress.png", url:"/"},
+    {title: "Drupal", image: "/images/worklogos/drupal.png", url:"/"},
+    {title: "ReactJS", image: "/images/worklogos/reactjs.png", url:"/"},
+    {title: "AngularJS", image: "/images/worklogos/angularjs.png", url:"/"},
+    {title: "NodeJS", image: "/images/worklogos/nodejs.png", url:"/"},
+    {title: "GatsbyJS", image: "/images/worklogos/gatsbyjs.png", url:"/"},
+    {title: "GraphQL", image: "/images/worklogos/graphql.png", url:"/"},
+    {title: "Netlify", image: "/images/worklogos/netlify.png", url:"/"},
+    {title: "Contentful", image: "/images/worklogos/contentful.png", url:"/"},
+  ];
   return (
     <Layout>
       <SEO title="Home" />
@@ -42,6 +44,17 @@ const IndexPage = (props) => {
         <div className='container'>
           <h2 className="title has-text-centered mb-6">Some of our awesome features</h2>
           <Offerings gridItems={allServices}/>
+          <h2 className="title has-text-centered mb-1 mt-4">Tech Stack</h2>
+          <h4 className="sub-title has-text-centered mb-5">Some of the major technology stacks, we expertise in...</h4>
+          <div className='columns box is-multiline is-centered is-vcentered'>
+            {logos && logos.map((item)=>(
+              <div className='column is-2 is-narrow has-text-centered'>
+              <figure className="image is-128x128">
+                <img src={item.image} title={item.title} />
+              </figure>
+            </div>
+            ))}  
+          </div>
         </div>
       </section>
     </Layout>
